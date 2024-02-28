30e3246f4f672d54deae1cf85d8fe12a5e24145c
Miralem.Ba <miralembanovi@gmail.com>
2024-02-28 18:32:34 +0100

    Function to add a meal

--- src/index.js
+++ src/index.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const caloriesInput = document.querySelector('input[type="number"]');
     const saveButton = document.querySelector('.bg-green-500');
     const totalCaloriesElement = document.querySelector('p');
-});
+
+    
+    function addMeal() {
+        const mealName = mealInput.value;
+        const calories = parseInt(caloriesInput.value);
+        if (!mealName || isNaN(calories)) {
+          alert('Bitte geben Sie einen Mahlzeit-Namen und Kalorien ein.');
+          return;
+        }

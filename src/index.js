b77d9968b554f42dd22b2e641c6d6270c237baeb
Miralem.Ba <miralembanovi@gmail.com>
2024-02-28 18:31:20 +0100

    References to the HTML elements

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const mealInput = document.querySelector('input[type="text"]');
+    const caloriesInput = document.querySelector('input[type="number"]');
+    const saveButton = document.querySelector('.bg-green-500');
+    const totalCaloriesElement = document.querySelector('p');
+});
